fceceb8796e696c11ee4d53d61aaa136f3df6501
williammtanaka <william.tanakinha@hotmail.com>
2016-09-08 22:02:41 -0300

    Update fila.h

--- fila.h
+++ fila.h
@@ -8,14 +8,16 @@ struct node {
 };
 
 typedef struct {
-  link maisAntigo;
-  link maisNovo;
+  link maisEsquerda;
+  link maisDireita;
 } * FILA;
 
 link novoNo(int item, link next);
 FILA novaFila();
-void inserir(FILA f, int e);
-int remover(FILA f);
+void inserirComeco(FILA f, int e);
+int removerComeco(FILA f);
+void inserirFinal(FILA f, int e);
+int removerFinal(FILA f);
 void imprimirFila(FILA f);
 void destroiFila(FILA f);
 int filaVazia(FILA f); 
